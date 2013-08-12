4b25f8ce4f61adf513752a92a12ec41415b165f9
kometchtech <kometch@gmail.com>
2013-08-13 05:58:50 +0900

    Update zabbix2.sh
    EnableRemoteCommands=1への書き換え

--- zabbix2.sh
+++ zabbix2.sh
@@ -14,6 +14,7 @@ ZBX_RC=/usr/local/etc/rc.d
 sed -i '' -e 's/Server=127.0.0.1/Server={ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
 sed -i '' -e 's/ServerActive=127.0.0.1/ServerActive={ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
 sed -i '' -e 's/Hostname=Zabbix\ server/Hostname={SERVER_NAME}/' ${ZBX_PASS}/zabbix_agentd.conf
+sed -i "" -e 's/\#\ EnableRemoteCommands=0/EnableRemoteCommands=1/' ${ZBX_PASS}/zabbix_agentd.conf
 
 # auto start config
 echo "zabbix_agentd_enable=\"YES\"" >> /etc/rc.conf
