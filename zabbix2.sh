fdea7e0e93280f7ecd5731041cb2555529ece27c
kometchtech <kometch@gmail.com>
2013-08-13 04:44:55 +0900

    Update zabbix2.sh

--- zabbix2.sh
+++ zabbix2.sh
@@ -11,9 +11,9 @@ ZBX_RC=/usr/local/etc/rc.d
 /bin/cp ${ZBX_PASS}/zabbix_agentd.conf.sample ${ZBX_PASS}/zabbix_agentd.conf
 
 # Value reWritable
-sed -i s/Server=127.0.0.1/Server={SERVER_IP}/g ${ZBX_PASS}/zabbix_agentd.conf
-sed -i s/ServerActive=127.0.0.1/ServerActive={SERVER_IP}/g ${ZBX_PASS}/zabbix_agentd.conf
-sed -i s/Hostname=Zabbix\ server/Hostname=josephine.kometch.local/g ${ZBX_PASS}/zabbix_agentd.conf
+sed -i '' -e 's/Server=127.0.0.1/Server={ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
+sed -i '' -e 's/ServerActive=127.0.0.1/ServerActive={ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
+sed -i '' -e 's/Hostname=Zabbix\ server/Hostname={SERVER_NAME}/' ${ZBX_PASS}/zabbix_agentd.conf
 
 # auto start config
 echo "zabbix_agentd_enable=\"YES\"" >> /etc/rc.conf
