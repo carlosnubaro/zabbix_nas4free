c8791d3d792dd0dc2282682dfb179552951051e6
kometchtech <kometch@gmail.com>
2013-08-16 22:03:21 +0900

    Update zabbix2.sh
    スクリプトの全面的な見直し。簡素化。

--- zabbix2.sh
+++ zabbix2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Valiable
-ZBX_PASS=/usr/local/etc/zabbix2
+ZBX_CONF=/usr/local/etc/zabbix2/zabbix_agentd.conf
 ZBX_RC=/usr/local/etc/rc.d
 ZABBIX_SERVER_IP=""
 SERVER_NAME=""
@@ -9,17 +9,17 @@ SERVER_NAME=""
 # add Zabbix-agent
 /usr/sbin/pkg_add -r zabbix2-agent
 
-# copy configure 
-/bin/cp ${ZBX_PASS}/zabbix_agentd.conf.sample ${ZBX_PASS}/zabbix_agentd.conf
+# copy configure
+/bin/cp ${ZBX_CONF}.sample ${ZBX_CONF}
 
 # Value reWritable
-sed -i '' -e 's/Server=127.0.0.1/Server=${ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
-sed -i '' -e 's/ServerActive=127.0.0.1/ServerActive=${ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
-sed -i '' -e 's/Hostname=Zabbix\ server/Hostname=${SERVER_NAME}/' ${ZBX_PASS}/zabbix_agentd.conf
-sed -i "" -e 's/\#\ EnableRemoteCommands=0/EnableRemoteCommands=1/' ${ZBX_PASS}/zabbix_agentd.conf
+sed -i '' -e 's/Server=127.0.0.1/Server='${ZABBIX_SERVER_IP}'/' ${ZBX_CONF}
+sed -i '' -e 's/ServerActive=127.0.0.1/ServerActive='${ZABBIX_SERVER_IP}'/' ${ZBX_CONF}
+sed -i '' -e 's/Hostname=Zabbix\ server/Hostname='${SERVER_NAME}'/' ${ZBX_CONF}
+sed -i '' -e 's/\#\ EnableRemoteCommands=0/EnableRemoteCommands=1/' ${ZBX_CONF}
 
 # auto start config
-# echo "zabbix_agentd_enable=\"YES\"" >> /etc/rc.conf
+#echo "zabbix_agentd_enable=\"YES\"" >> /etc/rc.conf
 
 # Zabbix agent start
 ${ZBX_RC}/zabbix_agentd start
