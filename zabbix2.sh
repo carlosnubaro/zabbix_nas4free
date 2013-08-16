ee72d7345f47a7766021675c2cc799e7dffd7c1a
kometchtech <kometch@gmail.com>
2013-08-16 10:58:55 +0900

    Update zabbix2.sh

--- zabbix2.sh
+++ zabbix2.sh
@@ -13,9 +13,9 @@ SERVER_NAME=""
 /bin/cp ${ZBX_PASS}/zabbix_agentd.conf.sample ${ZBX_PASS}/zabbix_agentd.conf
 
 # Value reWritable
-sed -i '' -e 's/Server=127.0.0.1/Server={ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
-sed -i '' -e 's/ServerActive=127.0.0.1/ServerActive={ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
-sed -i '' -e 's/Hostname=Zabbix\ server/Hostname={SERVER_NAME}/' ${ZBX_PASS}/zabbix_agentd.conf
+sed -i '' -e 's/Server=127.0.0.1/Server=${ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
+sed -i '' -e 's/ServerActive=127.0.0.1/ServerActive=${ZABBIX_SERVER_IP}/' ${ZBX_PASS}/zabbix_agentd.conf
+sed -i '' -e 's/Hostname=Zabbix\ server/Hostname=${SERVER_NAME}/' ${ZBX_PASS}/zabbix_agentd.conf
 sed -i "" -e 's/\#\ EnableRemoteCommands=0/EnableRemoteCommands=1/' ${ZBX_PASS}/zabbix_agentd.conf
 
 # auto start config
